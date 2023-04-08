c2c25220d378aa3f27cbc1b64510ee750a38b10b
arsscriptum <guillaumeplante.qc@gmail.com>
2023-04-08 10:03:35 -0400

    auto

--- NativeTests/NativeTests.cpp
+++ NativeTests/NativeTests.cpp
@@ -2,15 +2,50 @@
 //
 
 #include "stdafx.h"
-
+#include <Windows.h>
 #include "ServiceController.h"
 #include "ServiceEnumerator.h"
 
 #include <iostream>
 
+#define SCC_CONTROL_STOP                     SERVICE_CONTROL_STOP
+#define SCC_CONTROL_PAUSE                    SERVICE_CONTROL_PAUSE
+#define SCC_CONTROL_CONTINUE                 SERVICE_CONTROL_CONTINUE
+#define SCC_CONTROL_INTERROGATE              SERVICE_CONTROL_INTERROGATE
+#define SCC_CONTROL_SHUTDOWN                 SERVICE_CONTROL_SHUTDOWN
+#define SCC_CONTROL_PARAMCHANGE              SERVICE_CONTROL_PARAMCHANGE
+#define SCC_CONTROL_NETBINDADD               SERVICE_CONTROL_NETBINDADD
+#define SCC_CONTROL_NETBINDREMOVE            SERVICE_CONTROL_NETBINDREMOVE
+#define SCC_CONTROL_NETBINDENABLE            SERVICE_CONTROL_NETBINDENABLE
+#define SCC_CONTROL_NETBINDDISABLE           SERVICE_CONTROL_NETBINDDISABLE
+#define SCC_CONTROL_DEVICEEVENT              SERVICE_CONTROL_DEVICEEVENT
+#define SCC_CONTROL_HARDWAREPROFILECHANGE    SERVICE_CONTROL_HARDWAREPROFILECHANGE
+#define SCC_CONTROL_POWEREVENT               SERVICE_CONTROL_POWEREVENT
+#define SCC_CONTROL_SESSIONCHANGE            SERVICE_CONTROL_SESSIONCHANGE
+#define SCC_CONTROL_PRESHUTDOWN              SERVICE_CONTROL_PRESHUTDOWN
+#define SCC_CONTROL_TIMECHANGE               SERVICE_CONTROL_TIMECHANGE
+#define SCC_CONTROL_TRIGGEREVENT             SERVICE_CONTROL_TRIGGEREVENT
+#define SCC_CONTROL_LOWRESOURCES             SERVICE_CONTROL_LOWRESOURCES
+#define SCC_CONTROL_SYSTEMLOWRESOURCES       SERVICE_CONTROL_SYSTEMLOWRESOURCES
+
+#define SCC_CONTROL_USER_COMMAND_01			 0x00000081
+#define SCC_CONTROL_USER_COMMAND_02			 0x00000082
+#define SCC_CONTROL_USER_COMMAND_03			 0x00000083
+#define SCC_CONTROL_USER_COMMAND_04			 0x00000084
+#define SCC_CONTROL_USER_COMMAND_05			 0x00000085
+#define SCC_CONTROL_USER_COMMAND_06			 0x00000086
+#define SCC_CONTROL_USER_COMMAND_07			 0x00000087
+#define SCC_CONTROL_USER_COMMAND_08			 0x00000088
+
+
+int send(int cmd);
 int main()
 {
-   auto services = ServiceEnumerator::EnumerateServices();
+
+    send(SCC_CONTROL_USER_COMMAND_03);
+    Sleep(2000);
+    return 0;
+  /* auto services = ServiceEnumerator::EnumerateServices();
    for (auto const & s : services)
    {
       std::wcout << "Name:    " << s.ServiceName << std::endl
@@ -24,6 +59,7 @@ int main()
 
    auto print_status = [&service]() {
       std::wcout << "Status:              " << ServiceStatusToString(service.GetStatus()) << std::endl; 
+      
    };
 
    auto print_config = [](ServiceConfig const config) {
@@ -100,7 +136,61 @@ int main()
 
       print_status();
    }
-
+   */
    return 0;
 }
 
+
+int send(int cmd)
+{
+    SC_HANDLE managerHandle;
+    SC_HANDLE serviceHandle;
+
+    SERVICE_STATUS   controlParms;
+    DWORD retStatus;
+
+    managerHandle = OpenSCManager(NULL, NULL, GENERIC_READ);
+    if (NULL != managerHandle)
+    {
+        serviceHandle = OpenService(managerHandle, L"_dev_service_10", SERVICE_ALL_ACCESS);
+
+        if (NULL != serviceHandle)
+        {
+            cout << "connected to Service _dev_service_10" << endl;
+            BOOL success = ::ControlService(serviceHandle, cmd, &controlParms);
+            if (!success)
+            {
+                DWORD le = ::GetLastError();
+                cout << "ControlService failed " << le << endl;
+                switch (le)
+                {
+                case ERROR_ACCESS_DENIED:
+                case ERROR_DEPENDENT_SERVICES_RUNNING:
+                case ERROR_INVALID_HANDLE:
+                case ERROR_INVALID_PARAMETER:
+                case ERROR_INVALID_SERVICE_CONTROL:
+                case ERROR_SERVICE_CANNOT_ACCEPT_CTRL:
+                case ERROR_SERVICE_REQUEST_TIMEOUT:
+                case ERROR_SHUTDOWN_IN_PROGRESS:
+                    return FALSE;
+
+                case ERROR_SERVICE_NOT_ACTIVE:
+                default:
+                    break;
+                }
+            }
+            CloseServiceHandle(serviceHandle);
+        }
+        else
+        {
+            cout << "could not connect to Service" << endl;
+        }
+
+        CloseServiceHandle(managerHandle);
+    }
+    else
+    {
+        cout << "could not open service manager" << endl;
+    }
+    return 0;
+}
---
+++ ServiceLib/ServiceUserDefines.h
@@ -0,0 +1,26 @@
+﻿
+/*
+ *   𝓐𝓡𝓢 𝓢𝓒𝓡𝓘𝓟𝓣𝓤𝓜
+ *   🇧​​​​​🇾​​​​​ 🇬​​​​​🇺​​​​​🇮​​​​​🇱​​​​​🇱​​​​​🇦​​​​​🇺​​​​​🇲​​​​​🇪​​​​​🇵​​​​​🇱​​​​​🇦​​​​​🇳​​​​​🇹​​​​​🇪​​​​​.🇶​​​​​🇨​​​​​@🇬​​​​​🇲​​​​​🇦​​​​​🇮​​​​​🇱​​​​​.🇨​​​​​🇴​​​​​🇲​​​​​
+*/
+
+#ifndef __SERVICE_USER_DEFINE_H__
+#define __SERVICE_USER_DEFINE_H__
+
+#include "stdafx.h"
+
+#include <windows.h>
+#include <Winsvc.h>
+
+
+
+#define SERVICE_CONTROL_USER_COMMAND_01			0x00010000
+#define SERVICE_CONTROL_USER_COMMAND_02			0x00020000
+#define SERVICE_CONTROL_USER_COMMAND_03			0x00040000
+#define SERVICE_CONTROL_USER_COMMAND_04			0x00080000
+#define SERVICE_CONTROL_USER_COMMAND_05			0x00100000
+#define SERVICE_CONTROL_USER_COMMAND_06			0x00200000
+#define SERVICE_CONTROL_USER_COMMAND_07			0x00400000
+#define SERVICE_CONTROL_USER_COMMAND_08			0x00800000
+
+#endif //__SERVICE_USER_DEFINE_H__
